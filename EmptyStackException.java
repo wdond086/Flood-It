634e30737d0e102220f1fd19338b698452f79d26
Wilfried Dondji <tdondjiwilfried@hotmail.com>
2018-08-20 16:58:45 -0400

    first commit

---
+++ EmptyStackException.java
@@ -0,0 +1,14 @@
+// Author 1: Saheed Akinbile Abimbola
+
+// Author 2: Wilfried Tanezick Dondji Fogang
+
+// Course: ITI 1121-B
+// Assignment 4
+// Question 1
+
+public class EmptyStackException extends IllegalStateException {
+
+	public EmptyStackException() {
+		super("The stack is empty!");
+	}
+}
